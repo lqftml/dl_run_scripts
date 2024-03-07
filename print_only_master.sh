ba20ee7e6e502e7292743fec2a3863fdfb372ebb
Denis Boyda <boyda@polaris-login-03.hsn.cm.polaris.alcf.anl.gov>
2024-03-07 17:41:43 +0000

    Redirect of all but master rank print into /dev/null is added

---
+++ print_only_master.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -z "$OMPI_COMM_WORLD_RANK" ]; then
+    export RANK=$OMPI_COMM_WORLD_RANK
+elif [ ! -z "$PMI_RANK" ]; then
+    export RANK=$PMI_RANK
+else
+    echo "Error: no OpenMPI or MPICH found."
+    exit 1
+fi
+
+
+
+
+#https://stackoverflow.com/a/28099707/7674852
+if [ $RANK -eq 0 ]
+then
+  $@
+else
+  $@ >& /dev/null
+fi
