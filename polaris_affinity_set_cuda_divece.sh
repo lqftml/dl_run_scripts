800b0e028d90d20dd34ee6334be350bf2419c4a7
Denis Boyda <boyda@polaris-login-03.hsn.cm.polaris.alcf.anl.gov>
2024-03-08 15:13:44 +0000

    Affinity changed

--- polaris_affinity_set_cuda_divece.sh
+++ polaris_affinity_set_cuda_divece.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 case "${PMI_LOCAL_RANK}" in
 0)
+	export CUDA_VISIBLE_DEVICES=0
         exec numactl --physcpubind=24-31,56-63 --membind=3 $@
         ;;
 1)
+	export CUDA_VISIBLE_DEVICES=1
         exec numactl --physcpubind=16-23,48-55 --membind=2 $@
         ;;
 2)
+	export CUDA_VISIBLE_DEVICES=2
         exec numactl --physcpubind=8-15,40-47 --membind=1  $@
         ;;
 3)
+	export CUDA_VISIBLE_DEVICES=3
         exec numactl --physcpubind=0-7,32-39 --membind=0 $@
         ;;
 esac
